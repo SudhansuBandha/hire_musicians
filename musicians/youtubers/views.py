60db068dd750085223e6dc1dd01e96eaf0c19226
SudhansuBandha <sudhansu9.vssut@gmail.com>
2020-12-21 22:14:54 +0530

    Youtubers Home Page Modified

---
+++ musicians/youtubers/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def youtubers(request, id):
+    pass
+
+def youtubers_detail(request):
+    pass
+
+def search(request):
+    pass
