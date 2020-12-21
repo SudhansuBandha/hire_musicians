bc30a59fea6431eb58b5686d59bf4f2e10c2b8a2
SudhansuBandha <sudhansu9.vssut@gmail.com>
2020-12-21 12:03:16 +0530

    team updated

--- musicians/webpages/admin.py
+++ musicians/webpages/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
-from .models import Slider
+from .models import *
+
+class TeamAdmin(admin.ModelAdmin):
+    
+    def myphoto(self, object):
+        return format_html('<img src="{}" width="40" />'.format(object.photo.url))
+
+    list_display=('id','myphoto','first_name','role','created_date')
+    
+    #this will make first_name to be a clickable
+    list_display_links =('first_name','id')
+
+    #fields for searching based on parameter
+    search_fields = ('first_name', 'role')
+
+    #field for filtering based on the parameter
+    list_filter = ('role',)
+
 
 admin.site.register(Slider)
+admin.site.register(Team, TeamAdmin)
