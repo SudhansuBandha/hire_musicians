16a8fd3f09ecad039a9157c438e6c989ae9ac643
SudhansuBandha <sudhansu9.vssut@gmail.com>
2020-12-22 20:35:38 +0530

    First Phase Added

---
+++ musicians/hiremusicians/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Hiremusicians
+# Register your models here.
+
+admin.site.register(Hiremusicians)
--- musicians/musicians/settings.py
+++ musicians/musicians/settings.py
@@ -32,6 +32,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # Application definition
 
 INSTALLED_APPS = [
+    'hiremusicians.apps.HiremusiciansConfig',
     'accounts.apps.AccountsConfig',
     'youtubers.apps.YoutubersConfig',
     'webpages.apps.WebpagesConfig',
